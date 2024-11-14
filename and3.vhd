ba6ca14c700a9e5b1a16fa1a795e6b82f8017ac9
vw75092 <vw75092@umbc.edu>
2024-11-14 14:12:26 -0500

    Update and3.vhd

--- and3.vhd
+++ and3.vhd
@@ -1,43 +1,20 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 09/12/2024 09:16:37 PM
--- Design Name: 
--- Module Name: and3 - structural
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
+-- 3 input AND
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity and3 is
---  Port ( );
+port 
+( a : in std_logic;
+  b : in std_logic;
+  c : in std_logic;
+  y : out std_logic
+);
 end and3;
 
 architecture structural of and3 is
 
 begin
 
-
+y <= a AND b AND c;
+  
 end structural;
